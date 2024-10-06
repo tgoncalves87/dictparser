3e15688ebf78a74a596a2bacf4c9eac3b93c7031
Tiago Gonçalves <tgoncalves87@tiagogoncalves.pt>
2024-10-06 02:03:48 +0100

    Fix pylint plugin to recognise the function form of the dictparser decorator

--- src/dictparser/pylint.py
+++ src/dictparser/pylint.py
@@ -21,10 +21,12 @@ def transform(cls):
 
     for node in cls.decorators.nodes:
         if isinstance(node, astroid.Call):  # decorator with arguments
-            pass
+            node_to_infer = node.func
+        else:
+            node_to_infer = node
 
         try:
-            inferred = next(node.infer())
+            inferred = next(node_to_infer.infer())
         except (astroid.InferenceError, StopIteration):
             inferred = astroid.Uninferable
 
